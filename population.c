3ef605f89734a66cebee5701ec1ae8a9a6c9cf65
Angelica <Angelica137@users.noreply.github.com>
2021-02-21 16:13:23 +0000

    add semi colons and f to print statement

--- population.c
+++ population.c
@@ -9,15 +9,22 @@ int main(void)
     {
         n = get_int("What is the starting population? ");
 		}
-    while (n < 1);
+    while (n < 9);
     // TODO: Prompt for end size
     int ni;
     do
     {
         ni = get_int("What is the final population size? ");
 		}
-    while (ni < 9);
+    while (ni < n);
     // TODO: Calculate number of years until we reach threshold
-
+    int years = 0;
+    do
+    {
+        n = n + (n/3) - (n/4);
+        years++;
+		}
+    while (n < ni);
     // TODO: Print number of years
+    printf("The number is %i\n", years);
 }
