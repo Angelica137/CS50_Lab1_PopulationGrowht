e46447396078a1750d318c16ea19d3657ddb3b9e
Angelica <Angelica137@users.noreply.github.com>
2021-02-21 15:03:33 +0000

    get user input

--- population.c
+++ population.c
@@ -4,9 +4,19 @@
 int main(void)
 {
     // TODO: Prompt for start size
-
+    int n;
+    do
+    {
+        n = get_int("What is the starting population? ");
+		}
+    while (n < 1);
     // TODO: Prompt for end size
-
+    int ni;
+    do
+    {
+        ni = get_int("What is the final population size? ");
+		}
+    while (ni < 9);
     // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
